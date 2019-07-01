244b128ccc67bcbe47c832e9a82a5b34697372a1
Anton Popoff <panton295@gmail.com>
2019-07-01 17:36:08 +0300

    Add MainFragment

---
+++ app/src/main/java/com/apandroid/colorwheelapp/MainFragment.kt
@@ -0,0 +1,29 @@
+package com.apandroid.colorwheelapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        previewScreenButton.setOnClickListener { replaceFragment(PreviewFragment()) }
+        testColorWheelScreenButton.setOnClickListener {  }
+        testAlphaSeekBarScreen.setOnClickListener {  }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        fragmentManager?.beginTransaction()
+            ?.replace(R.id.container, fragment)
+            ?.addToBackStack(null)
+            ?.commit()
+    }
+}
