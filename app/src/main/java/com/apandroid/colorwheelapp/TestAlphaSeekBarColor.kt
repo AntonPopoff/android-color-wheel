513a30e9280171d01b214f2a73adbcaeba7d07e6
Anton Popoff <panton295@gmail.com>
2019-07-02 16:08:31 +0300

    Implement screen for testing AlphaSeekBar

--- app/src/main/java/com/apandroid/colorwheelapp/TestAlphaSeekBarColor.kt
+++ app/src/main/java/com/apandroid/colorwheelapp/TestAlphaSeekBarColor.kt
@@ -1,16 +1,24 @@
 package com.apandroid.colorwheelapp
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
 import com.apandroid.colorwheelapp.extensions.density
 import com.apandroid.colorwheelapp.extensions.afterTextChanged
+import com.apandroid.colorwheelapp.extensions.setOnProgressChangeListener
+import com.colorwheelapp.colorwheel.alphaseekbar.AlphaSeekBar
+import com.colorwheelapp.colorwheel.alphaseekbar.setAlphaSilently
 import kotlinx.android.synthetic.main.fragment_test_alpha_seek_bar.*
+import java.util.*
 import kotlin.math.roundToInt
 
 class TestAlphaSeekBarColor : Fragment() {
+
+    private val random by lazy { Random() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_test_alpha_seek_bar, container, false)
@@ -20,32 +28,62 @@ class TestAlphaSeekBarColor : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         barSizeEdit.afterTextChanged(this::onBarSizeChanged)
-        barSizeEdit.setText((libAlphaSeekBar.barSize / density).roundToInt().toString())
+        barSizeEdit.setText((alphaSeekBar.barSize / density).roundToInt().toString())
 
         cornerRadiusEdit.afterTextChanged(this::onCornerRadiusChanged)
-        cornerRadiusEdit.setText((libAlphaSeekBar.cornersRadius / density).roundToInt().toString())
+        cornerRadiusEdit.setText((alphaSeekBar.cornersRadius / density).roundToInt().toString())
 
         thumbRadiusEdit.afterTextChanged(this::onThumbRadiusChanged)
-        thumbRadiusEdit.setText((libAlphaSeekBar.thumbRadius / density).roundToInt().toString())
+        thumbRadiusEdit.setText((alphaSeekBar.thumbRadius / density).roundToInt().toString())
 
-        alphaSeekBar.progress = libAlphaSeekBar.alphaValue
-        alphaText.text = getString(R.string.alpha_template, libAlphaSeekBar.alphaValue)
+        alphaSeekBarController.progress = alphaSeekBar.alphaValue
+        alphaSeekBarController.setOnProgressChangeListener(this::onAlphaSeekBarControllerChanged)
+        alphaText.text = getString(R.string.alpha_template, alphaSeekBar.alphaValue)
+
+        alphaSeekBar.alphaChangeListener = this::onAlphaSeekBarChange
 
         orientationRadioGroup.check(R.id.verticalOrientationButton)
+        orientationRadioGroup.setOnCheckedChangeListener(this::onOrientationChange)
         
-        randomizeRgbButton.setOnClickListener {  }
-        randomizeArgbButton.setOnClickListener {  }
+        randomizeRgbButton.setOnClickListener { randomizeRgb() }
+        randomizeArgbButton.setOnClickListener { randomizeArgb() }
     }
 
     private fun onBarSizeChanged(s: String) {
-        libAlphaSeekBar.barSize = ((s.toIntOrNull() ?: 0) * density).roundToInt()
+        alphaSeekBar.barSize = ((s.toIntOrNull() ?: 0) * density).roundToInt()
     }
 
     private fun onCornerRadiusChanged(s: String) {
-        libAlphaSeekBar.cornersRadius = (s.toIntOrNull() ?: 0) * density
+        alphaSeekBar.cornersRadius = (s.toIntOrNull() ?: 0) * density
     }
 
     private fun onThumbRadiusChanged(s: String) {
-        libAlphaSeekBar.thumbRadius = ((s.toIntOrNull() ?: 0) * density).roundToInt()
+        alphaSeekBar.thumbRadius = ((s.toIntOrNull() ?: 0) * density).roundToInt()
+    }
+
+    private fun onAlphaSeekBarControllerChanged(progress: Int) {
+        alphaText.text = getString(R.string.alpha_template, alphaSeekBar.alphaValue)
+        alphaSeekBar.setAlphaSilently(progress)
+    }
+
+    private fun onAlphaSeekBarChange(alpha: Int) {
+        alphaText.text = getString(R.string.alpha_template, alphaSeekBar.alphaValue)
+        alphaSeekBarController.progress = alpha
+    }
+
+    private fun onOrientationChange(group: RadioGroup, checkedId: Int) {
+        if (checkedId == R.id.verticalOrientationButton) {
+            alphaSeekBar.orientation = AlphaSeekBar.Orientation.VERTICAL
+        } else if (checkedId == R.id.horizontalOrientationButton) {
+            alphaSeekBar.orientation = AlphaSeekBar.Orientation.HORIZONTAL
+        }
+    }
+
+    private fun randomizeRgb() {
+        alphaSeekBar.rgb = Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255))
+    }
+
+    private fun randomizeArgb() {
+        alphaSeekBar.argb = Color.argb(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255))
     }
 }
