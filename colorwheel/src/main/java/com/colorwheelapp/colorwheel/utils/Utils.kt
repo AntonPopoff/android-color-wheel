ea1982efbb9e1e9fd3cf73c78bd95699ef0e3c12
Anton Popov <panton295@gmail.com>
2019-07-09 20:41:04 +0300

    Refactoring

--- colorwheel/src/main/java/com/colorwheelapp/colorwheel/utils/Utils.kt
+++ colorwheel/src/main/java/com/colorwheelapp/colorwheel/utils/Utils.kt
@@ -1,11 +1,7 @@
 package com.colorwheelapp.colorwheel.utils
 
-fun <T> ensureNumberWithinRange(value: T, start: T, end: T): T where T : Number, T : Comparable<T> {
-    if (start > end) throw IllegalArgumentException("Start must be lower than end. (start = $start, end = $end)")
-
-    return when {
-        value < start -> start
-        value > end -> end
-        else -> value
-    }
+fun <T> ensureNumberWithinRange(value: T, start: T, end: T): T where T : Number, T : Comparable<T> = when {
+    value < start -> start
+    value > end -> end
+    else -> value
 }
