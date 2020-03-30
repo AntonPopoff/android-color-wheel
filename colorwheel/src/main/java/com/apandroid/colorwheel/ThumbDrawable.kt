23bae21fb314a6480d40bda615b459af5b178062
Anton Popoff <panton295@gmail.com>
2020-03-30 23:52:54 +0300

    Refactoring

--- colorwheel/src/main/java/com/apandroid/colorwheel/ThumbDrawable.kt
+++ colorwheel/src/main/java/com/apandroid/colorwheel/ThumbDrawable.kt
@@ -1,7 +1,6 @@
 package com.apandroid.colorwheel
 
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Rect
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.ShapeDrawable
