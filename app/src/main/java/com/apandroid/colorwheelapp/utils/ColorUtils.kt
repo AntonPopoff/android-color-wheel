cc6e864c998bec293bd3e749a80946f9f8713678
Anton Popoff <panton295@gmail.com>
2020-04-03 22:24:33 +0300

    Add test functions to TestGradientSeekBarFragment

--- app/src/main/java/com/apandroid/colorwheelapp/utils/ColorUtils.kt
+++ app/src/main/java/com/apandroid/colorwheelapp/utils/ColorUtils.kt
@@ -4,3 +4,5 @@ import android.graphics.Color
 import java.util.*
 
 fun randomArgb(random: Random) = Color.argb(random.nextInt(256), random.nextInt(256), random.nextInt(256), random.nextInt(256))
+
+fun randomRgb(random: Random) = Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255))
