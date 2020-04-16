8614461071adb363a2c61ab369ecb90599c7778d
Anton Popoff <panton295@gmail.com>
2020-04-16 11:47:36 +0300

    ThumbDrawable code clean up

--- colorwheel/src/main/java/com/apandroid/colorwheel/ThumbDrawable.kt
+++ colorwheel/src/main/java/com/apandroid/colorwheel/ThumbDrawable.kt
@@ -9,12 +9,6 @@ internal class ThumbDrawable {
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply { strokeWidth = 1f }
 
-    var bounds = Rect()
-        set(value) { field.set(value) }
-
-    var colorCircleScale = 0f
-        set(value) { field = ensureNumberWithinRange(value, 0f, 1f) }
-
     var indicatorColor = 0
 
     var strokeColor = 0
@@ -23,35 +17,44 @@ internal class ThumbDrawable {
 
     var radius = 0
 
+    var bounds = Rect()
+        set(value) { field.set(value) }
+
+    var colorCircleScale = 0f
+        set(value) { field = ensureNumberWithinRange(value, 0f, 1f) }
+
     fun setBounds(thumbX: Int, thumbY: Int) {
         bounds.set(thumbX - radius, thumbY - radius, thumbX + radius, thumbY + radius)
     }
 
     fun draw(canvas: Canvas) {
-        drawThumb(canvas)
-        drawStroke(canvas)
-        drawColorIndicator(canvas)
+        val cx = bounds.exactCenterX()
+        val cy = bounds.exactCenterY()
+
+        drawThumb(canvas, cx, cy)
+        drawStroke(canvas, cx, cy)
+        drawColorIndicator(canvas, cx, cy)
     }
 
-    private fun drawThumb(canvas: Canvas) {
+    private fun drawThumb(canvas: Canvas, cx: Float, cy: Float) {
         paint.color = thumbColor
         paint.style = Paint.Style.FILL
-        canvas.drawCircle(bounds.exactCenterX(), bounds.exactCenterY(), radius.toFloat(), paint)
+        canvas.drawCircle(cx, cy, radius.toFloat(), paint)
     }
 
-    private fun drawStroke(canvas: Canvas) {
+    private fun drawStroke(canvas: Canvas, cx: Float, cy: Float) {
         val strokeCircleRadius = radius - paint.strokeWidth / 2f
 
         paint.color = strokeColor
         paint.style = Paint.Style.STROKE
-        canvas.drawCircle(bounds.exactCenterX(), bounds.exactCenterY(), strokeCircleRadius, paint)
+        canvas.drawCircle(cx, cy, strokeCircleRadius, paint)
     }
 
-    private fun drawColorIndicator(canvas: Canvas) {
+    private fun drawColorIndicator(canvas: Canvas, cx: Float, cy: Float) {
         val colorIndicatorCircleRadius = radius * colorCircleScale
 
         paint.color = indicatorColor
         paint.style = Paint.Style.FILL
-        canvas.drawCircle(bounds.exactCenterX(), bounds.exactCenterY(), colorIndicatorCircleRadius, paint)
+        canvas.drawCircle(cx, cy, colorIndicatorCircleRadius, paint)
     }
 }
