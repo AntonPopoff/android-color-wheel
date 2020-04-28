cabe1cf5fc3849cf8a972b3e2685f56a5b769b80
Anton Popoff <panton295@gmail.com>
2020-04-28 15:49:22 +0300

    Implement ThumbDrawableState class

--- colorwheel/src/main/java/com/apandroid/colorwheel/ThumbDrawable.kt
+++ colorwheel/src/main/java/com/apandroid/colorwheel/ThumbDrawable.kt
@@ -2,6 +2,8 @@ package com.apandroid.colorwheel
 
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.os.Parcel
+import android.os.Parcelable
 import com.apandroid.colorwheel.utils.ensureNumberWithinRange
 
 internal class ThumbDrawable {
@@ -49,5 +51,54 @@ internal class ThumbDrawable {
         paint.color = indicatorColor
         paint.style = Paint.Style.FILL
         canvas.drawCircle(x, y, colorIndicatorCircleRadius, paint)
+    }
+
+    fun restoreState(parcelable: Parcelable) {
+        (parcelable as? ThumbDrawableState)?.let {
+            radius = it.radius
+            thumbColor = it.thumbColor
+            strokeColor = it.strokeColor
+            colorCircleScale = it.colorCircleScale
+        }
+    }
+
+    fun saveState(): Parcelable = ThumbDrawableState(this)
+}
+
+private class ThumbDrawableState(
+    val radius: Int,
+    val thumbColor: Int,
+    val strokeColor: Int,
+    val colorCircleScale: Float
+) : Parcelable {
+
+    constructor(thumbDrawable: ThumbDrawable) : this(
+        thumbDrawable.radius,
+        thumbDrawable.thumbColor,
+        thumbDrawable.strokeColor,
+        thumbDrawable.colorCircleScale
+    )
+
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readFloat()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(radius)
+        parcel.writeInt(thumbColor)
+        parcel.writeInt(strokeColor)
+        parcel.writeFloat(colorCircleScale)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<ThumbDrawableState> {
+
+        override fun createFromParcel(parcel: Parcel) = ThumbDrawableState(parcel)
+
+        override fun newArray(size: Int) = arrayOfNulls<ThumbDrawableState>(size)
     }
 }
