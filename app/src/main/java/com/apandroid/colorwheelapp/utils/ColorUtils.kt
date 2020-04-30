ab805594a18d16d509ca55528826babab26d05d9
Anton Popoff <panton295@gmail.com>
2020-04-30 18:44:23 +0300

    Fix ColorWheel's saturation colors

--- app/src/main/java/com/apandroid/colorwheelapp/utils/ColorUtils.kt
+++ app/src/main/java/com/apandroid/colorwheelapp/utils/ColorUtils.kt
@@ -6,3 +6,10 @@ import java.util.*
 fun randomArgb(random: Random) = Color.argb(random.nextInt(256), random.nextInt(256), random.nextInt(256), random.nextInt(256))
 
 fun randomRgb(random: Random) = Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255))
+
+fun setColorAlpha(argb: Int, alpha: Int) = Color.argb(
+    alpha,
+    Color.red(argb),
+    Color.green(argb),
+    Color.blue(argb)
+)
--- colorwheel/src/main/java/com/apandroid/colorwheel/utils/ColorUtils.kt
+++ colorwheel/src/main/java/com/apandroid/colorwheel/utils/ColorUtils.kt
@@ -5,13 +5,16 @@ import kotlin.math.roundToInt
 
 internal const val MAX_ALPHA = 255
 
-fun setColorAlpha(argb: Int, alpha: Int) = clearColorAlpha(argb) or (alpha shl 24)
-
-internal fun clearColorAlpha(argb: Int): Int = (argb shl 8 ushr 8)
-
 internal fun interpolateColorLinear(startColor: Int, endColor: Int, offset: Float) = Color.argb(
     (Color.alpha(startColor) + offset * (Color.alpha(endColor) - Color.alpha(startColor))).roundToInt(),
     (Color.red(startColor) + offset * (Color.red(endColor) - Color.red(startColor))).roundToInt(),
     (Color.green(startColor) + offset * (Color.green(endColor) - Color.green(startColor))).roundToInt(),
     (Color.blue(startColor) + offset * (Color.blue(endColor) - Color.blue(startColor))).roundToInt()
+)
+
+internal fun setColorAlpha(argb: Int, alpha: Int) = Color.argb(
+    alpha,
+    Color.red(argb),
+    Color.green(argb),
+    Color.blue(argb)
 )
