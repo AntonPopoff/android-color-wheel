a2c1ca50287c7d3e6762d2046d76a6d69355484a
Anton Popoff <panton295@gmail.com>
2020-07-29 23:52:19 +0300

    Move EmptyTextWather class to listeners package

--- app/src/main/java/com/apandroid/colorwheelapp/extensions/android/views/EditTextExtensions.kt
+++ app/src/main/java/com/apandroid/colorwheelapp/extensions/android/views/EditTextExtensions.kt
@@ -1,17 +1,8 @@
 package com.apandroid.colorwheelapp.extensions.android.views
 
 import android.text.Editable
-import android.text.TextWatcher
 import android.widget.EditText
-
-interface EmptyTextWatcher : TextWatcher {
-
-    override fun afterTextChanged(s: Editable) { }
-
-    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) { }
-
-    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) { }
-}
+import com.apandroid.colorwheelapp.utils.listeners.EmptyTextWatcher
 
 fun EditText.afterTextChanged(listener: (String) -> Unit) {
     this.addTextChangedListener(object : EmptyTextWatcher {
