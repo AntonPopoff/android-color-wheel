609cf408ad554d2bd12f0b2126b9f2efa73a0081
Anton Popoff <panton295@gmail.com>
2020-10-22 00:09:03 +0300

    Removed unused imports

--- app/src/main/java/com/apandroid/colorwheelapp/MainFragment.kt
+++ app/src/main/java/com/apandroid/colorwheelapp/MainFragment.kt
@@ -1,9 +1,7 @@
 package com.apandroid.colorwheelapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.apandroid.colorwheelapp.colorwheeltest.TestColorWheelFragment
