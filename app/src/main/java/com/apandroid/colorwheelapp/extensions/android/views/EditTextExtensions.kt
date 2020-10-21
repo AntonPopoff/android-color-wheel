c57f2c64e4921d7a873ad587d4cd2547a380d21f
Anton Popoff <panton295@gmail.com>
2020-10-22 00:08:04 +0300

    Code style clean up

--- app/src/main/java/com/apandroid/colorwheelapp/extensions/android/views/EditTextExtensions.kt
+++ app/src/main/java/com/apandroid/colorwheelapp/extensions/android/views/EditTextExtensions.kt
@@ -6,11 +6,11 @@ import android.widget.EditText
 
 interface EmptyTextWatcher : TextWatcher {
 
-    override fun afterTextChanged(s: Editable) { }
+    override fun afterTextChanged(s: Editable) {}
 
-    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) { }
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
-    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) { }
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
 }
 
 fun EditText.afterTextChanged(listener: (String) -> Unit) {
--- app/src/main/java/com/apandroid/colorwheelapp/utils/listeners/EmptySeekBarChangeListener.kt
+++ app/src/main/java/com/apandroid/colorwheelapp/utils/listeners/EmptySeekBarChangeListener.kt
@@ -4,9 +4,9 @@ import android.widget.SeekBar
 
 interface EmptySeekBarChangeListener : SeekBar.OnSeekBarChangeListener {
 
-    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) { }
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {}
 
-    override fun onStartTrackingTouch(seekBar: SeekBar) { }
+    override fun onStartTrackingTouch(seekBar: SeekBar) {}
 
-    override fun onStopTrackingTouch(seekBar: SeekBar) { }
+    override fun onStopTrackingTouch(seekBar: SeekBar) {}
 }
